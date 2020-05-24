0da48429b3d3859ea9510aaf1fa5a95053e01335
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-25 06:58:52 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="container-fluid">
+        <div class="footer">
+            <ul class="footer-list" id="horizontal-list">
+                <li><a class="footer-text left" href="index.html">Stella Maris</a></li>
+                <li><a class="footer-text left" href="our-services.html">Our Services</a></li>
+                <li><a class="footer-text left" href="directions.html">Directions</a></li>
+                <li><a class="footer-text left" href="rights.html">Rights</a></li>
+                <li><a class="footer-text left" href="health.html">Health</a></li>
+                <li><a class="footer-text footer-logo" href="Index.html"> <img class="footer-logo-center" src="assets/logo_stellamaris.png" alt="Stella Maris Logo"> </a></li>
+                <li><a class="footer-text" href="directions.html">16 Queen Victoria Street, Fremantle WA 6160</a></li>
+                <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
+                <li><a class="btn btn-default footer supportus-btn" href="support-us.html">Support Us</a></li>
+            </ul>
+                        <div class="footer-text copyright">
+            <small>&copy; Copyright 2020, Stella Maris Seafarers' Centre Fremantle</small>
+        </div> <!--COPYRIGHT-->
+        </div>
+        <!--footer-->
+    </footer>
+
+</body>
+
+</html>
