108401ae874360bd82ca5a164d5d8144c1d02432
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-26 07:40:51 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
                 <li><a class="footer-text footer-logo" href="Index.html"> <img class="footer-logo-center" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/logo_stellamaris.png" alt="Stella Maris Logo"> </a></li>
                 <li><a class="footer-text" href="directions.html">16 Queen Victoria Street, Fremantle WA 6160</a></li>
                 <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
-                <li><a class="btn btn-default footer supportus-btn" href="support-us.html">Support Us</a></li>
+                <li><a class="btn btn-default footer supportus-btn" href="http://206.189.45.97/~mesh21/sub/?page_id=158">Support Us</a></li>
             </ul>
             <div class="footer-text copyright">
             <small>&copy; Copyright 2020, Stella Maris Seafarers' Centre Fremantle</small>
