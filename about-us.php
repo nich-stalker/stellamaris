4626f05ac410ffdc7e50472ec30c7737e2f93ba0
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-26 07:11:57 +0800

    Update about-us.php

--- about-us.php
+++ about-us.php
@@ -3,20 +3,17 @@
 Template Name: About us
 */
 ?>
-<?php get_header(); /* Tells WordPress to include header.php */ ?><section class="container-fluid about-us">
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<section class="container-fluid about-us">
         <div class="row">
             <div class="col-sm-6">
                 <div class="about-us-content">
                     <div class="about-us-title">
-                        <h3>About Stella Maris</h3>
+                        <h3><?php the_field('titleforaboutus'); ?></h3>
                     </div>
                     <!--ABOUT-US-TITLE-->
                     <div class="about-us-bodycopu">
-                        <p>The Apostleship of the Sea is a Catholic charity operating through Stella Maris centres and supporting seafarers worldwide.</p>
-                        <p>We provide practical and pastoral care to all seafarers, regardless of nationality, belief or race. Our port chaplains and volunteer ship visitors welcome seafarers, offer welfare services and advice, practical help, care and friendship. </p>
-                        <p>The Stella Maris Seafarers’ Centre in Fremantle is part of an international network known to the maritime world as Stella Maris, working in 334 ports with 227 port chaplains in 59 countries. </p>
-                        <p>90% of world trade is transported by ship. However the life of a modern seafarer can be dangerous and lonely. They may spend up to a year at a time away from home, separated from their family and loved ones and often working in harsh conditions.</p>
-                        <p>The Apostleship of the Sea relies wholly on voluntary contributions. We are only able to continue our work through the generous donations of our supporters and volunteers.</p>
+                        <p><?php the_field('textforaboutus'); ?></p>
                     </div>
                     <!--ABOUT-US-BODYCOPY-->
                 </div>
@@ -24,7 +21,7 @@ Template Name: About us
             </div>
             <!--COL-SM-6-->
             <div class="col-sm-6">
-                <img class="jesus_seafarer" src="assets/jesus_seafarer.png" alt="Jesus standing over the seafarer">
+                <img class="jesus_seafarer" alt="Jesus standing over the seafarer" src="<?php the_field('imageforaboutus'); ?>">
             </div>
             <!--COL-SM-6-->
         </div>
@@ -36,43 +33,21 @@ Template Name: About us
     <section class="container-fluid directions subpage">
         <div class="row">
             <div class="col-sm-5 map">
-                <img class="minimal-map-freo" src="assets/minimal_map_freo.png" alt="Map of Fremantle">
+                <img class="minimal-map-freo" alt="Map of Fremantle" src="<?php the_field('imagefordirections'); ?>">
             </div>
             <!--COL-SM-6-->
             <div class="col-sm-7 directions_bodycopy">
                 <div class="directions_text">
-                    <h3> Where are we located?</h3>
-                    <p> Stella Maris is based at 16 Queen Victoria Street in Frematnle. Click get directions to find out exactly where we are or see all the useful things seafarers may need around us.</p>
-                    <a class="btn btn-default ourservices_btn" href="directions.html">Get Directions</a>
+                    <h3><?php the_field('titlefordirections'); ?></h3>
+                    <p><?php the_field('textfordirections'); ?></p>
+                    <div class="aboutusbutton">
+                    <a class="btn btn-default ourservices_btn" href="directions.html"><?php the_field('buttonfordirections'); ?></a>
                 </div>
+                    </div>
                 <!--DIRECTIONS_TEXT-->
             </div>
             <!--COL-SM-6-->
         </div>
         <!--ROW-->
     </section>
-    <!--CONTAINER-FLUID DIRECTIONS-->
-
-
-    <!--FOOTER-->
-
-    <footer class="container-fluid">
-        <div class="footer">
-            <ul class="footer-list" id="horizontal-list">
-                <li><a class="footer-text left" href="index.html">Stella Maris</a></li>
-                <li><a class="footer-text left" href="our-services.html">Our Services</a></li>
-                <li><a class="footer-text left" href="directions.html">Directions</a></li>
-                <li><a class="footer-text left" href="rights.html">Rights</a></li>
-                <li><a class="footer-text left" href="#">Health</a></li>
-                <li><a class="footer-text footer-logo" href="Index.html"> <img class="footer-logo-center" src="assets/logo_stellamaris.png" alt="Stella Maris Logo"> </a></li>
-                <li><a class="footer-text" href="directions.html">16 Queen Victoria Street, Fremantle WA 6160</a></li>
-                <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
-                <li><a class="btn btn-default footer supportus-btn" href="support-us.html">Support Us</a></li>
-            </ul>
-        </div>
-        <!--footer-->
-    </footer>
-
-</body>
-
-</html>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
