06ce9152b4a1808bcb0896d341a4b281c5ae66b5
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-25 12:33:32 +0800

    Create support-us.php

---
+++ support-us.php
@@ -0,0 +1,84 @@
+<?php get_header(); /* Tells WordPress to include header.php */ ?>
+<section class="container-fluid support-us">
+        <div class="support-us-section">
+            <div class="support-us-header">
+                <h3>Support us today</h3>
+                <p>Stella Maris is a “not-for-profit organisation staffed chiefly by volunteers.</p>
+                <p> We need your prayers, your support and your time to help us to continue offering support to seafarers.</p>
+            </div>
+            <!--SUPPORT-US-HEADER-->
+            <div class="row">
+
+                <div class="col-sm-6">
+                    <div class="icons">
+                        <img class="donation-white" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/donation_white.png" alt="An icon of donation">
+                    </div>
+                    <!--ICONs-->
+                    <div class="donation_btn">
+                        <a class="btn btn-default donation_btn" href="donate.html">Donate Now</a>
+                    </div>
+                    <!--DONATION_BTN-->
+                    <div class="donation_text">
+                        <p>Seafarers spend most of their life at sea and are unable to access stores easily. Any donations are welcome, from clothing to magazines, to games or any lose change.</p>
+                    </div>
+                    <!--DONATION_TEXT-->
+                </div>
+                <!--COL-SM-6-->
+
+                <div class="col-sm-6">
+                    <div class="icons">
+                        <img class="volunteer-white" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/volunteer_white.png" alt="An icon of volunteering">
+                    </div>
+                    <!--ICONS-->
+                    <div Class="volunteer_btn">
+                        <a class="btn btn-default volunteer_btn" href="volunteer.html">Volunteer Now</a>
+                    </div>
+                    <!--VOLUNTEER_BTN-->
+                    <div class="volunteer_text">
+                        <p>Stella Maris provides a number of services to fully assist seafarers during their stay in Fremantle. Volunteers with a friendly attitude and smile are always needed to make this possible.</p>
+                    </div>
+                    <!--VOLUNTEER_TEXT-->
+                </div>
+                <!--COL-SM-6-->
+            </div>
+            <!--ROW-->
+        </div>
+        <!--SUPPORT-US-SECTION-->
+    </section>
+    <!--CONTAINER-FLUID SUPPORT-US-->
+
+
+    <!--IMAGE AND TITLE-->
+
+    <section class="image-title">
+        <div class="row">
+            <div class="col-sm-6 supportus-img">
+
+            </div>
+            <!--COL-SM-6-->
+            <div class="col-sm-6">
+                <div class="support-seafarers">
+                    <h1>Help out the </h1>
+                    <h1> seafarers today!</h1>
+                </div>
+                <!--SUPPORT-SEAFARERS-->
+            </div>
+            <!--COL-SM-6-->
+        </div>
+        <!--ROW-->
+    </section>
+
+
+    <!--BIOGRAPHY-->
+
+    <section class="biography">
+        <div class="biography-title">
+            <h3>Why should you help?</h3>
+        </div>
+        <!--BIOGRAPHY-TITLE-->
+        <div class="biography-bodycopy">
+            <p>40’000 seafarers, most of whom come from underdeveloped countries visit our port every year. They bring with them essential goods to Australia, things that we need to be able to function and survive. We owe them the basic goods and services that they requite to be able to contact their loved ones, feed and clothe themselves. Without your help we at Stella Maris would struggle to be able to assist these them.</p>
+        </div>
+        <!--BIOGRAPHY-BODYCOPY-->
+    </section>
+<?php get_footer();?>
