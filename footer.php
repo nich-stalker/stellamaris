0d3769dc40887ad8a6e0c72d0213df7cb416d98d
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-27 06:58:22 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <footer class="container-fluid">
         <div class="footer">
             <ul class="footer-list" id="horizontal-list">
-                <li><a class="footer-text left" href="index.html">Stella Maris</a></li>
-                <li><a class="footer-text left" href="our-services.html">Our Services</a></li>
-                <li><a class="footer-text left" href="directions.html">Directions</a></li>
-                <li><a class="footer-text left" href="rights.html">Rights</a></li>
-                <li><a class="footer-text left" href="health.html">Health</a></li>
+                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=234">Stella Maris</a></li>
+                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=166">Our Services</a></li>
+                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=167">Directions</a></li>
+                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=168">Rights</a></li>
+                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=170">Health</a></li>
                 <li><a class="footer-text footer-logo" href="Index.html"> <img class="footer-logo-center" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/logo_stellamaris.png" alt="Stella Maris Logo"> </a></li>
-                <li><a class="footer-text" href="directions.html">16 Queen Victoria Street, Fremantle WA 6160</a></li>
+                <li><a class="footer-text" href="http://206.189.45.97/~mesh21/sub/?page_id=167">16 Queen Victoria Street, Fremantle WA 6160</a></li>
                 <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
                 <li><a class="btn btn-default footer supportus-btn" href="http://206.189.45.97/~mesh21/sub/?page_id=158">Support Us</a></li>
             </ul>
