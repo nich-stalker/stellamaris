d0314ba950abfb4721e8cc4235a5579247de4b61
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-26 18:55:32 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ function theme_add_bootstrap() {
     wp_enqueue_script( 'bootstrap-cdn-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js' );
 wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'test', get_stylesheet_directory_uri() . '/responsive.css' );
-wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js', array(), null, false);
+wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), null, false);
 wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.4.1', true );
 }
  
