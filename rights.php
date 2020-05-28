24507b9930763eaa9e2271a9aecf9eb296ed6bc2
rickyperdec <rickyperdec@gmail.com>
2020-05-28 09:39:59 +0800

    Update rights.php

--- rights.php
+++ rights.php
@@ -25,11 +25,11 @@ Template Name: Rights
             <p class="rights-title rights-margin"><?php the_field('titleforrightslinkone'); ?></p>
             <p class="rights-free rights-margin"><?php the_field('textforrightslinkone'); ?></p>
 
-            <a class="rights-link rights-margin"><?php the_field('linkforrightslinkone'); ?></a>
+            <a class="rights-link rights-margin" href="https://www.seafarerhelp.org"><?php the_field('linkforrightslinkone'); ?></a>
 
             <p class="rights-title rights-margin"><?php the_field('titleforrightslinktwo'); ?></p>
 
-            <a class="rights-link rights-margin"><?php the_field('linkforrightslinktwo'); ?></a>
+            <a class="rights-link rights-margin" href="http://www.amsa.gov.au"><?php the_field('linkforrightslinktwo'); ?></a>
 
             </div>
             <!--ROW-->
