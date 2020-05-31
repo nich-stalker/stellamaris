dd2128775131883c7213e87d197ea3c970bde26c
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-05-31 14:14:39 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
 <footer class="footer">
         <div class="container-fluid footer">
             <ul class="footer-list" id="horizontal-list">
-                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=234">Stella Maris</a></li>
-                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=166">Our Services</a></li>
-                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=167">Directions</a></li>
-                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=168">Rights</a></li>
-                <li><a class="footer-text left" href="http://206.189.45.97/~mesh21/sub/?page_id=170">Health</a></li>
-                <li><a class="footer-text footer-logo" href="http://206.189.45.97/~mesh21/sub/?page_id=234"> <img class="footer-logo-center" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/logo_stellamaris.png" alt="Stella Maris Logo" href="http://206.189.45.97/~mesh21/sub/?page_id=234"> </a></li>
-                <li><a class="footer-text" href="http://206.189.45.97/~mesh21/sub/?page_id=167">16 Queen Victoria Street, Fremantle WA 6160</a></li>
-                <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
-                <li><a class="btn btn-default footer supportus-btn" href="http://206.189.45.97/~mesh21/sub/?page_id=158">Support Us</a></li>
+                <li><a class="footer-text left" href="<?php echo get_permalink(234); ?>">Stella Maris</a></li>
+                <li><a class="footer-text left" href="<?php echo get_permalink(166); ?>">Our Services</a></li>
+                <li><a class="footer-text left" href="<?php echo get_permalink(167); ?>">Directions</a></li>
+                <li><a class="footer-text left" href="<?php echo get_permalink(168); ?>">Rights</a></li>
+                <li><a class="footer-text left" href="<?php echo get_permalink(170); ?>">Health</a></li>
+                <li><a class="footer-text footer-logo" href="<?php echo get_permalink(234); ?>"> <img class="footer-logo-center" img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/logo_stellamaris.png" alt="Stella Maris Logo" href="<?php echo get_permalink(234); ?>"> </a></li>
+                <li><a class="footer-text" href="<?php echo get_permalink(167); ?>">16 Queen Victoria Street, Fremantle WA 6160</a></li>
+                <li><a class="footer-text facebook-logo" href="https://www.facebook.com/stellamarisfremantle"> <img class="facebook-logo-footer" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/facebook_logo.png" alt="Facebook Logo"> </a></li>
+                <li><a class="btn btn-default footer supportus-btn" href="<?php echo get_permalink(158); ?>">Support Us</a></li>
             </ul>
             <div class="footer-text copyright">
             <small>&copy; Copyright 2020, Stella Maris Seafarers' Centre Fremantle</small>
