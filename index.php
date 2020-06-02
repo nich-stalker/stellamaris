70a621f28eb33ae0953bda0a0dec3c01f47170ee
nich-stalker <61613752+nich-stalker@users.noreply.github.com>
2020-06-02 19:10:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-<section class="container-fluid banner">
-        <div class="herobg">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=8');//look for posts that have the category of 8
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="our-services-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?> 
-
-</div>
+    <section class="container-fluid banner herobg">
+        <div class="">
+            <h1> Stella Maris Seafarers' Centre Fremantle </h1>
+            <h2 class="herobg-subhead">Supporting Seafarers from all across the world</h2>
+            <div class="ourservices-btn">
+                <a class="btn btn-default ourservices_btn" href="<?php echo get_permalink(166); ?>">Our Services</a>
+            </div>
+            <!--OURSERVICES_BTN-->
+        </div>
+        <!--HEROBG-->
     </section>
 
 
@@ -24,23 +18,17 @@ wp_reset_query();?>
     <section class="container-fluid directions">
         <div class="row">
             <div class="col-sm-5 map">
-                <img class="minimal-map-freo" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/minimal_map_freo.png" alt="Map of Fremantle">
+                <img class="minimal-map-freo" src="assets/minimal_map_freo.png" alt="Map of Fremantle">
             </div>
             <!--COL-SM-6-->
             <div class="col-sm-7 directions_bodycopy">
                 <div class="directions_text">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=9');//look for posts that have the category of 9
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="directions-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
+                    <h3> Where are we located?</h3>
+                    <p> Stella Maris is based at 16 Queen Victoria Street in Frematnle. Click get directions to find out exactly where we are or see all the useful things seafarers may need around us.</p>
+                    <div class="getdirections-btn">
+                        <a class="btn btn-default ourservices_btn" href="<?php echo get_permalink(167); ?>">Get Directions</a>
+                    </div>
+                    <!--GETDIRECTIONS-BTN-->
                 </div>
                 <!--DIRECTIONS_TEXT-->
             </div>
@@ -57,59 +45,42 @@ wp_reset_query();?>
     <section class="container-fluid support-us">
         <div class="support-us-section">
             <div class="support-us-header">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=7');//look for posts that have the category of 7
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="support-us-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
+                <h3>Support us today</h3>
+                <p>Stella Maris is a “not-for-profit" organisation staffed chiefly by volunteers.</p>
+                <p> We need your prayers, your support and your time to help us to continue offering support to seafarers.</p>
             </div>
             <!--SUPPORT-US-HEADER-->
             <div class="row">
 
                 <div class="col-sm-6">
                     <div class="icons">
-                        <img class="donation-white" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/donation_white.png" alt="An icon of donation">
+                        <img class="donation-white" src="assets/donation_white.png" alt="An icon of donation">
                     </div>
                     <!--ICONs-->
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=10');//look for posts that have the category of 10
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="donate-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
+                    <div class="donation_btn">
+                        <a class="btn btn-default donation_btn" href="<?php echo get_permalink(371); ?>">Donate Now</a>
+                    </div>
+                    <!--DONATION_BTN-->
+                    <div class="donation_text">
+                        <p>Seafarers spend most of their life at sea and are unable to access stores easily. Any donations are welcome, from clothing to magazines, to games or any lose change.</p>
+                    </div>
+                    <!--DONATION_TEXT-->
                 </div>
                 <!--COL-SM-6-->
 
                 <div class="col-sm-6">
                     <div class="icons">
-                        <img class="volunteer-white" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/volunteer_white.png" alt="An icon of volunteering">
+                        <img class="volunteer-white" src="assets/volunteer_white.png" alt="An icon of volunteering">
                     </div>
                     <!--ICONS-->
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=11');//look for posts that have the category of 11
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="volunteer-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
+                    <div Class="volunteer_btn">
+                        <a class="btn btn-default volunteer_btn" href="<?php echo get_permalink(350); ?>">Volunteer Now</a>
+                    </div>
+                    <!--VOLUNTEER_BTN-->
+                    <div class="volunteer_text">
+                        <p>Stella Maris provides a number of services to fully assist seafarers during their stay in Fremantle. Volunteers with a friendly attitude and smile are always needed to make this possible.</p>
+                    </div>
+                    <!--VOLUNTEER_TEXT-->
                 </div>
                 <!--COL-SM-6-->
             </div>
@@ -128,7 +99,7 @@ wp_reset_query();?>
             <div class="row">
                 <div class="col-sm-5">
                     <div class="seafarers-rights-img">
-                        <img class="seafarers-rights" src="http://206.189.45.97/~mesh21/sub/wp-content/themes/stellamaris/assets/seafarers_rights.jpg" alt="Smiling seafarer">
+                        <img class="seafarers-rights" src="assets/seafarers_rights.jpg" alt="Smiling seafarer">
                     </div>
                     <!--SEAFARERS-RIGHTS-IMG-->
                 </div>
@@ -136,18 +107,12 @@ wp_reset_query();?>
                 <div class="col-sm-7">
                     <div class="rights_content">
                         <div class="seafarers-rights-text">
-<?php
-global $more;//define a global variable
-$more = 0;// the global varibale is now equal to 0
-query_posts('cat=12');//look for posts that have the category of 12
-if(have_posts()) ://if we have posts to display
-while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
-?>
-            <div><p class="seafarer-rights-home"><?php the_content() ?></p></div>
-<?php
-endwhile;
-endif;
-wp_reset_query();?>
+                            <h3>Seafarer Rights</h3>
+                            <p class="seafarers-rights-home"> It is important for seafarers to be aware of their rights whilst at sea and also when they are in port. We have provided the basic information needed and links to other important sources.</p>
+                            <div class="seafarers-rights_btn">
+                                <a class="btn btn-default seafarers-rights_btn"  href="<?php echo get_permalink(168); ?>">View Rights</a>
+                            </div>
+                            <!--SEAFARERS-RIGHTS-TEXT-->
                         </div>
                         <!--SEAFARERS-RIGHTS_BTN-->
                     </div>
@@ -159,4 +124,5 @@ wp_reset_query();?>
         </div>
         <!--RIGHTS-->
     </section>
+    <!--CONTAINER-FLUID SEAFARERS-RIGHTS-->
 <?php get_footer();?>
