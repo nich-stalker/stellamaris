0a0e3ff7d8e350cf3a423be063022b573dfdbe2b
rickyperdec <rickyperdec@gmail.com>
2020-06-02 18:33:47 +0800

    Update directions.php

--- directions.php
+++ directions.php
@@ -13,164 +13,20 @@ Template Name: Directions
                 <img class="google-map" alt="Qries" src="<?php the_field('directionsmap'); ?>"
                  width="600">
             </a>
-            
-            
-            
-            
-            
-            
-            
-            <style type="text/css">
-.acf-map {
-    width: 100%;
-    height: 400px;
-    border: #ccc solid 1px;
-    margin: 20px 0;
-}
-
-// Fixes potential theme css conflict.
-.acf-map img {
-   max-width: inherit !important;
-}
-</style>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC3Vg95Dchqc0CaHNl9R19BjunATqKSTpA"></script>
-<script type="text/javascript">
-(function( $ ) {
-
-/**
- * initMap
- *
- * Renders a Google Map onto the selected jQuery element
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   jQuery $el The jQuery element.
- * @return  object The map instance.
- */
-function initMap( $el ) {
-
-    // Find marker elements within map.
-    var $markers = $el.find('.marker');
-
-    // Create gerenic map.
-    var mapArgs = {
-        zoom        : $el.data('zoom') || 16,
-        mapTypeId   : google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map( $el[0], mapArgs );
-
-    // Add markers.
-    map.markers = [];
-    $markers.each(function(){
-        initMarker( $(this), map );
-    });
-
-    // Center map based on markers.
-    centerMap( map );
-
-    // Return map instance.
-    return map;
-}
-
-/**
- * initMarker
- *
- * Creates a marker for the given jQuery element and map.
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   jQuery $el The jQuery element.
- * @param   object The map instance.
- * @return  object The marker instance.
- */
-function initMarker( $marker, map ) {
-
-    // Get position from marker.
-    var lat = $marker.data('-32.0490723');
-    var lng = $marker.data('115.747941');
-    var latLng = {
-        lat: parseFloat( lat ),
-        lng: parseFloat( lng )
-    };
-
-    // Create marker instance.
-    var marker = new google.maps.Marker({
-        position : latLng,
-        map: map
-    });
-
-    // Append to reference for later use.
-    map.markers.push( marker );
-
-    // If marker contains HTML, add it to an infoWindow.
-    if( $marker.html() ){
-
-        // Create info window.
-        var infowindow = new google.maps.InfoWindow({
-            content: $marker.html()
-        });
-
-        // Show info window when marker is clicked.
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open( map, marker );
-        });
-    }
-}
-
-/**
- * centerMap
- *
- * Centers the map showing all markers in view.
- *
- * @date    22/10/19
- * @since   5.8.6
- *
- * @param   object The map instance.
- * @return  void
- */
-function centerMap( map ) {
-
-    // Create map boundaries from all map markers.
-    var bounds = new google.maps.LatLngBounds();
-    map.markers.forEach(function( marker ){
-        bounds.extend({
-            lat: marker.position.lat(),
-            lng: marker.position.lng()
-        });
-    });
-
-    // Case: Single marker.
-    if( map.markers.length == 1 ){
-        map.setCenter( bounds.getCenter() );
-
-    // Case: Multiple markers.
-    } else{
-        map.fitBounds( bounds );
-    }
-}
-
-// Render maps on page load.
-$(document).ready(function(){
-    $('.acf-map').each(function(){
-        var map = initMap( $(this) );
-    });
-});
-
-})(jQuery);
-</script>
-<?php 
-$location = get_field('location');
-if( $location ): ?>
-    <div class="acf-map" data-zoom="16">
-        <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
-    </div>
-<?php endif; ?>
-<p class="pickup text-center"><?php the_field('textfordirectionsintro'); ?></p>
-    
-        
-        </div>
+          
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=750');//look for posts that have the category of 750
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+            <div><iframe class="stella-maris-map"><?php the_content() ?></iframe></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>
+</div>
     </section>.
 
 
