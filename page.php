1b51a794b8d06c43b58b87327ae8cbb9379e54d6
rickyperdec <rickyperdec@gmail.com>
2020-06-05 20:18:44 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,17 +1,16 @@
 <?php get_header(); /* Tells WordPress to include header.php */ ?>
-<div class="container-fluid maincontainer">
+<div class="container-fluid maincontainer text-center">
     <div class="container">
-        <div class="row">
-            <section class="col-sm-7 maintext">				
+        <div>
+            <section class="maintext text-center">				
     <article class="excerpts"><!----> <!--we can reuse the css on the old article to restyle the new dynamic posts-->
 	
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			
-<div class="post single-page">
-		<h3 class="posttitle" id="post-<?php the_ID(); ?>"><?php the_title(); ?></h3>
+<div class="post single-page text-center">
 
-		<div class="postcontent">
-			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'alignright')); ?><?php */?>
+		<div class="postcontent text-center">
+			<?php /*?><?php the_post_thumbnail(array(150,150), array ('class' => 'aligncenter')); ?><?php */?>
 			<?php the_content(); ?>
 		<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong>', 'after' => '</p>')); ?>
         </div><!--postcontet-->
